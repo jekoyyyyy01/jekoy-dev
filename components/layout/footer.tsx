de4934088aae6b61c80169ec006d08d3089dd1ca
Jericho <jekcosico@gmail.com>
2024-06-07 23:57:40 +0800

    hide the footer logo

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -26,7 +26,7 @@ export const Footer: React.FC<FooterProps> = (props) => {
           <Stack spacing="8">
             <Stack alignItems="flex-start">
               <Flex>
-                <Box as={siteConfig.logo} flex="1" height="32px" />
+                {/*<Box as="img" src={siteConfig.logo} alt="Logo" flex="1" height="32px" />*/}
               </Flex>
               <Text fontSize="md" color="muted">
                 {siteConfig.seo.description}
