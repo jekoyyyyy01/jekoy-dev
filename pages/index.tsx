e5cdf9a839cbb6b31e8c07e49720dbcf7a64b970
Jericho <jekcosico@gmail.com>
2024-06-08 01:16:56 +0800

    comment not needed

--- pages/index.tsx
+++ pages/index.tsx
@@ -92,7 +92,7 @@ const Home: NextPage = () => {
 
 const HeroSection: React.FC = () => {
   return (
-    <Box position="home" overflow="hidden">
+    <Box overflow="hidden">
       <BackgroundGradient height="100%" zIndex="-1" />
       <Container maxW="container.xl" pt={{ base: 40, lg: 60 }} pb="40">
         <Stack direction={{ base: "column", lg: "row" }} alignItems="center">
@@ -186,112 +186,6 @@ const HeroSection: React.FC = () => {
         reveal={FallInPlace}
       />*/}
     </Box>
-  );
-};
-
-const HighlightsSection = () => {
-  const { value, onCopy, hasCopied } = useClipboard("yarn add @saas-ui/react");
-
-  return (
-    <Highlights>
-      <HighlightsItem colSpan={[1, null, 2]} title="Core components">
-        <VStack alignItems="flex-start" spacing="8">
-          <Text color="muted" fontSize="xl">
-            Get started for free with <Em>30+ open source components</Em>.
-            Including authentication screens with Clerk, Supabase and Magic.
-            Fully functional forms with React Hook Form. Data tables with React
-            Table.
-          </Text>
-
-          <Flex
-            rounded="full"
-            borderWidth="1px"
-            flexDirection="row"
-            alignItems="center"
-            py="1"
-            ps="8"
-            pe="2"
-            bg="primary.900"
-            _dark={{ bg: "gray.900" }}
-          >
-            <Box>
-              <Text color="yellow.400" display="inline">
-                yarn add
-              </Text>{" "}
-              <Text color="cyan.300" display="inline">
-                @saas-ui/react
-              </Text>
-            </Box>
-            <IconButton
-              icon={hasCopied ? <FiCheck /> : <FiCopy />}
-              aria-label="Copy install command"
-              onClick={onCopy}
-              variant="ghost"
-              ms="4"
-              isRound
-              color="white"
-            />
-          </Flex>
-        </VStack>
-      </HighlightsItem>
-      <HighlightsItem title="Solid foundations">
-        <Text color="muted" fontSize="lg">
-          We don't like to re-invent the wheel, neither should you. We
-          selected the most productive and established tools in the scene and
-          build Saas UI on top of it.
-        </Text>
-      </HighlightsItem>
-      <HighlightsTestimonialItem
-        name="Renata Alink"
-        description="Founder"
-        avatar="/static/images/avatar.jpg"
-        gradient={["pink.200", "purple.500"]}
-      >
-        “Saas UI helped us set up a beautiful modern UI in no time. It saved us
-        hundreds of hours in development time and allowed us to focus on
-        business logic for our specific use-case from the start.”
-      </HighlightsTestimonialItem>
-      <HighlightsItem
-        colSpan={[1, null, 2]}
-        title="Start your next idea two steps ahead"
-      >
-        <Text color="muted" fontSize="lg">
-          We took care of all your basic frontend needs, so you can start
-          building functionality that makes your product unique.
-        </Text>
-        <Wrap mt="8">
-          {[
-            "authentication",
-            "navigation",
-            "crud",
-            "settings",
-            "multi-tenancy",
-            "layouts",
-            "billing",
-            "a11y testing",
-            "server-side rendering",
-            "documentation",
-            "onboarding",
-            "storybooks",
-            "theming",
-            "upselling",
-            "unit testing",
-            "feature flags",
-            "responsiveness",
-          ].map((value) => (
-            <Tag
-              key={value}
-              variant="subtle"
-              colorScheme="purple"
-              rounded="full"
-              px="3"
-            >
-              {value}
-            </Tag>
-          ))}
-        </Wrap>
-      </HighlightsItem>
-    </Highlights>
   );
 };
 
@@ -446,49 +340,155 @@ const SkillItem = ({ name, icon }) => {
   );
 };
 
-const TestimonialsSection = () => {
-  const columns = React.useMemo(() => {
-    return testimonials.items.reduce<Array<typeof testimonials.items>>(
-      (columns, t, i) => {
-        columns[i % 3].push(t);
+// const HighlightsSection = () => {
+//   const { value, onCopy, hasCopied } = useClipboard("yarn add @saas-ui/react");
 
-        return columns;
-      },
-      [[], [], []]
-    );
-  }, []);
+//   return (
+//     <Highlights>
+//       <HighlightsItem colSpan={[1, null, 2]} title="Core components">
+//         <VStack alignItems="flex-start" spacing="8">
+//           <Text color="muted" fontSize="xl">
+//             Get started for free with <Em>30+ open source components</Em>.
+//             Including authentication screens with Clerk, Supabase and Magic.
+//             Fully functional forms with React Hook Form. Data tables with React
+//             Table.
+//           </Text>
 
-  return (
-    <Testimonials
-      title={testimonials.title}
-      columns={[1, 2, 3]}
-      innerWidth="container.xl"
-    >
-      <>
-        {columns.map((column, i) => (
-          <Stack key={i} spacing="8">
-            {column.map((t, i) => (
-              <Testimonial key={i} {...t} />
-            ))}
-          </Stack>
-        ))}
-      </>
-    </Testimonials>
-  );
-};
+//           <Flex
+//             rounded="full"
+//             borderWidth="1px"
+//             flexDirection="row"
+//             alignItems="center"
+//             py="1"
+//             ps="8"
+//             pe="2"
+//             bg="primary.900"
+//             _dark={{ bg: "gray.900" }}
+//           >
+//             <Box>
+//               <Text color="yellow.400" display="inline">
+//                 yarn add
+//               </Text>{" "}
+//               <Text color="cyan.300" display="inline">
+//                 @saas-ui/react
+//               </Text>
+//             </Box>
+//             <IconButton
+//               icon={hasCopied ? <FiCheck /> : <FiCopy />}
+//               aria-label="Copy install command"
+//               onClick={onCopy}
+//               variant="ghost"
+//               ms="4"
+//               isRound
+//               color="white"
+//             />
+//           </Flex>
+//         </VStack>
+//       </HighlightsItem>
+//       <HighlightsItem title="Solid foundations">
+//         <Text color="muted" fontSize="lg">
+//           We don't like to re-invent the wheel, neither should you. We
+//           selected the most productive and established tools in the scene and
+//           build Saas UI on top of it.
+//         </Text>
+//       </HighlightsItem>
+//       <HighlightsTestimonialItem
+//         name="Renata Alink"
+//         description="Founder"
+//         avatar="/static/images/avatar.jpg"
+//         gradient={["pink.200", "purple.500"]}
+//       >
+//         “Saas UI helped us set up a beautiful modern UI in no time. It saved us
+//         hundreds of hours in development time and allowed us to focus on
+//         business logic for our specific use-case from the start.”
+//       </HighlightsTestimonialItem>
+//       <HighlightsItem
+//         colSpan={[1, null, 2]}
+//         title="Start your next idea two steps ahead"
+//       >
+//         <Text color="muted" fontSize="lg">
+//           We took care of all your basic frontend needs, so you can start
+//           building functionality that makes your product unique.
+//         </Text>
+//         <Wrap mt="8">
+//           {[
+//             "authentication",
+//             "navigation",
+//             "crud",
+//             "settings",
+//             "multi-tenancy",
+//             "layouts",
+//             "billing",
+//             "a11y testing",
+//             "server-side rendering",
+//             "documentation",
+//             "onboarding",
+//             "storybooks",
+//             "theming",
+//             "upselling",
+//             "unit testing",
+//             "feature flags",
+//             "responsiveness",
+//           ].map((value) => (
+//             <Tag
+//               key={value}
+//               variant="subtle"
+//               colorScheme="purple"
+//               rounded="full"
+//               px="3"
+//             >
+//               {value}
+//             </Tag>
+//           ))}
+//         </Wrap>
+//       </HighlightsItem>
+//     </Highlights>
+//   );
+// };
 
-const PricingSection = () => {
-  return (
-    <Pricing {...pricing}>
-      <Text p="8" textAlign="center" color="muted">
-        VAT may be applicable depending on your location.
-      </Text>
-    </Pricing>
-  );
-};
+// const TestimonialsSection = () => {
+//   const columns = React.useMemo(() => {
+//     return testimonials.items.reduce<Array<typeof testimonials.items>>(
+//       (columns, t, i) => {
+//         columns[i % 3].push(t);
 
-const FaqSection = () => {
-  return <Faq {...faq} />;
-};
+//         return columns;
+//       },
+//       [[], [], []]
+//     );
+//   }, []);
+
+//   return (
+//     <Testimonials
+//       title={testimonials.title}
+//       columns={[1, 2, 3]}
+//       innerWidth="container.xl"
+//     >
+//       <>
+//         {columns.map((column, i) => (
+//           <Stack key={i} spacing="8">
+//             {column.map((t, i) => (
+//               <Testimonial key={i} {...t} />
+//             ))}
+//           </Stack>
+//         ))}
+//       </>
+//     </Testimonials>
+//   );
+// };
+
+// const PricingSection = () => {
+//   return (
+//     <Pricing {...pricing}>
+//       <Text p="8" textAlign="center" color="muted">
+//         VAT may be applicable depending on your location.
+//       </Text>
+//     </Pricing>
+//   );
+// };
+
+// const FaqSection = () => {
+//   return <Faq {...faq} />;
+// };
 
 export default Home;
