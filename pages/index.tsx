c94008a51e1cd9b3b612400c95857949e49f8f25
Jericho Cosico <jekcosico@gmail.com>
2025-02-12 08:19:23 +0800

    add new image for header

--- pages/index.tsx
+++ pages/index.tsx
@@ -139,7 +139,7 @@ const HeroSection: React.FC = () => {
             <FallInPlace delay={1}>
               <Box overflow="hidden" height="50%">
                 <Image
-                  src=""
+                  src="/static/images/dev2.png"
                   loading="lazy"
                   width={700}
                   height={600}
